35644bd1480b13d70031fbc528cfce1362e590d8
pod1project <pod1finalproject@gmail.com>
2025-11-26 15:40:20 -0800

    Update backend to new S3 bucket

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "pod1-terraform-state"
+    bucket = "pod1-terraform-state-new"
     key    = "production/terraform.tfstate"
     region = "us-east-1"
   }
